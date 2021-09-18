c2a3dafb08201f6b2abd500ed55f67f2b27f1cfd
zafar2199 <75061841+zafar2199@users.noreply.github.com>
2021-09-18 13:48:04 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector(".btn");
 let text = document.querySelector(".text");
+let text2 = document.querySelector(".text2");
 
 
 
@@ -7,7 +8,7 @@ btn.addEventListener('click',()=>{
   //delfin haqida malumot
   const delfin = document.querySelector("select").value;
 
-  let tekshir = delfin=== "ha" ? text.innerHTML = `Sizning holatingiz : Trik `  : sTime()
+  let tekshir = delfin=== "ha" ? text.innerHTML = `Sizning holatingiz : Trik ! `  : sTime()
 
   function sTime() {
     //aculani kemagacha masofasi
@@ -27,19 +28,20 @@ btn.addEventListener('click',()=>{
 
   // aculani kemagacha boradigan vahti
   const aculaT = aculs/aculaV
-  console.log(aculaT)
+  console.log(`qacha vqtta borishi aculani: ${aculaT}`)
 
   // sizning kemagacha boradigan vahtingiz
   const myT =kemaS/myV;
-  console.log(myT)
+  console.log(`qacha vqtta borishingiz sizni: ${myT}`)
 
   // tirik yoki o'lik
 
   const top = aculaT - myT
 
-  let result = top>= 0 ? "Trik":"Halok bo'ldi"
+  let result = top>= 0 ? "Trik !":"Halok bo'ldi !"
   let text = document.querySelector(".text");
-  text.innerHTML = `Sizning holatingiz : ${result}`
+  text.innerHTML = `Sizning holatingiz : ${result}`;
+  text2.innerHTML = `aculani kemaga borish vahti: ${aculaT} va sizni kemaga borish vahtingiz: ${myT} `;
   // console.log(result)
 
   }
